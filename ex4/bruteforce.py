fe283a95b59239342a940e7989ae661d63f8dd78
Víctor Sánchez Gassull <vsanchez.gassull@gmail.com>
2018-11-20 21:01:18 +0100

    Update bruteforce.py

--- ex4/bruteforce.py
+++ ex4/bruteforce.py
@@ -3,7 +3,6 @@ import itertools
 import hashlib
 
 ll = ['s','3','m','1','c','!','S','M','C','=','%']
-# sha = 'c0c0f0f1db9807bdc0d4d4094ffac18085c2a955'
 sha = 'b8e46064c5cb98321ab378f546d2641881b43563'
 
 for L in range (0, len(ll)+1):
